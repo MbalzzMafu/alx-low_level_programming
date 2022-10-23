90697b871260aadb6a145c1de449adc848ed03a0
MbalzzMafu <mbalenhle.mafu@icloud.com>
2022-10-23 09:02:18 -0700

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - computes abs value of int
+ * @n: the int
+ *
+ * Return: the abs of the int
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
