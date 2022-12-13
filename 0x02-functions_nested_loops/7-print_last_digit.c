22d468184bef5d494e3c2e39c0f1026716bef2b7
MbalzzMafu <mbalenhle.mafu@icloud.com>
2022-12-13 09:46:26 -0800

    Task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,17 +2,18 @@
 
 /**
  * print_last_digit - prints the last digit
- * @n: the number
- * Return: value of the last digit
+ * @a: An input integer
+ * Return: value of the last digit n
  */
-int print_last_digit(int n)
+int print_last_digit(int a)
 {
-	int ld = n % 10;
+	int n;
 
-	if (ld < 0)
-		ld *= -1;
+	if (a < 0)
+		n = -1 * (a % 10);
+	else
+		n = a % 10;
+	_putchar((n % 10) + '0');
 
-	_putchar(ld + '0');
-
-	return (0);
+	return (n % 10);
 }
