a7a38e34129318060ea83940c3394e3270107e17
MbalzzMafu <mbalenhle.mafu@icloud.com>
2022-12-13 11:01:23 -0800

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - add 2 numbers
+ * @a: the first number
+ * @b: the second number
+ * Return: the total of both numbers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
