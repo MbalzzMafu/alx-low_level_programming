91ed5d625b69eb2663ef9ce1fa8f1bd9e4a990df
MbalzzMafu <mbalenhle.mafu@icloud.com>
2023-04-04 16:32:25 -0700

    Task 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,53 +1,40 @@
 #include <stdlib.h>
 #include "lists.h"
 /**
- * delete_nodeint_at_index - delete node at index
- * @head: head node
- * @index: index node
- * Return: 1 succed -1 fails
+ * delete_nodeint_at_index - deletes a node in a linked list at a certain index
+ * @head: pointer to the first element in the list
+ * @index: index of the node to delete
+ *
+ * Return: 1 (Success), or -1 (Fail)
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *prev, *node;
-
-	if (!*head)
-		return (-1);
-
-	prev = get_nodeint_at_index(*head, index - 1);
-	node = get_nodeint_at_index(*head, index);
-	if (!node)
-		return (-1);
-	if (index != 0)
-	{
-		prev->next = node->next;
-		node->next = 0;
-	}
-	else
-	{
-		*head = node->next;
-	}
-	free(node);
-	return (1);
-}
-/**
- * get_nodeint_at_index - get node at index
- * @head: head node
- * @index: index node
- * Return: address of index node
- */
-listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
-{
+	listint_t *temp = *head;
+	listint_t *current = NULL;
 	unsigned int i = 0;
 
-	if (!head)
-		return (0);
+	if (*head == NULL)
+		return (-1);
 
-		while (head)
-		{
-			if (index == i)
-				return (head);
-			i++;
-			head = head->next;
-}
-return (0);
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(temp);
+		return (1);
+	}
+
+	while (i < index - 1)
+	{
+		if (!temp || !(temp->next))
+			return (-1);
+		temp = temp->next;
+		i++;
+	}
+
+	current = temp->next;
+	temp->next = current->next;
+
+	free(current);
+
+	return (1);
 }
