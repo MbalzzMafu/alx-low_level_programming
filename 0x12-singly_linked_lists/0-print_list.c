57c5f2adfbb7fb98aa5c212892bad78bf062a30d
MbalzzMafu <mbalenhle.mafu@icloud.com>
2023-04-04 13:03:20 -0700

    Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,17 +10,19 @@
 
 size_t print_list(const list_t *h)
 {
-size_t s = 0;
+	size_t nodes = 0;
 
-while (h)
-{
-	if (!h->str)
-		printf("[0](nil)\n");
-	else
-		printf("[%u]%s\n", h->len, h->str);
-	h = h->next;
-	s++;
-}
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
 
-return (s);
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
+		nodes++;
+		h = h->next;
+	}
+
+	return (nodes);
 }
